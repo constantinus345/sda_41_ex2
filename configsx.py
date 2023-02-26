9c680355e91bd1b069a9ef73636908c5ffb8fa1c
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-02-26 12:20:42 +0200

    exercitiu

--- configsx.py
+++ configsx.py
@@ -1,4 +1,1 @@
-API_key_google_translate = 'AIzaSyDhLIObv0LJjmQc5yMwElfgspcnLKbcaj8'
-
-API_translate_json_secret_path = 'D:/Python_Code/creds/sdatests-379009-8ea0adfa31c8.json'
-API_open_AI_chat = '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'
+a=1
